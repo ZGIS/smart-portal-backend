ead240be07d8c1e529d0ca4f086e6a6dbcb6f796
Alexander Kmoch <allixender@googlemail.com>
2016-11-02 11:36:19 +1300

    docker preps

--- docker-preps.sh
+++ docker-preps.sh
@@ -3,7 +3,15 @@
 APPNAME=$(cat build.sbt  | egrep "^name := " | cut -d "=" -f 2 | sed "s/\"*//g" | sed "s/\ *//g")
 APPVERSION=$(cat build.sbt  | egrep "^version := " | cut -d "=" -f 2 | sed "s/\"*//g" | sed "s/\ *//g")
 
+pwd
+
+ls -lh target/universal
+
 cp Dockerfile target/universal
 cp portal-backend.k8s.yaml target/universal
 
+ls -lh target/universal
+
 cd target/universal && test -f ${APPNAME}-${APPVERSION}.tgz && tar -cvzf ${APPNAME}-${APPVERSION}-docker.tgz ${APPNAME}-${APPVERSION}.tgz Dockerfile
+
+ls -lh

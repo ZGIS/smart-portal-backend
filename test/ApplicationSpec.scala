79eb483f9041fb4b3d11f9c97052fbb7b10218f9
Alexander Kmoch <allixender@googlemail.com>
2016-11-07 20:15:02 +1300

    test with H2 DB, possibly should separate between most H2 testable things and a few Postgresql specific tests?

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,13 +1,71 @@
+import com.typesafe.config.ConfigFactory
+import org.scalatest.{BeforeAndAfter, TestData}
 import org.scalatestplus.play._
-import play.api.test._
+import play.api.db.evolutions._
+import play.api.db.{Database, Databases}
+import play.api.inject.guice._
 import play.api.test.Helpers._
+import play.api.test._
+import play.api.{Application, Configuration}
+
+/**
+  *
+  */
+trait WithTestDatabase {
+
+  /**
+    *
+    * @param block
+    * @tparam T
+    * @return
+    */
+  def withTestDatabase[T](block: Database => T) = {
+
+    val config = new Configuration(ConfigFactory.load("application-testdb.conf"))
+
+    val driver = config.getString("db.default.driver").get
+    val url = config.getString("db.default.url").get
+    val username = config.getString("db.default.username").get
+    val password = config.getString("db.default.password").get
+    val logSql = config.getBoolean("db.default.logSql").get
+
+    Databases.withDatabase(
+      driver = driver,
+      url = url,
+      config = Map(
+        "user" -> username,
+        "password" -> password,
+        "logStatements" -> logSql
+      )
+    ) { database =>
+
+      Evolutions.withEvolutions(database, ClassLoaderEvolutionsReader.forPrefix("test/")) {
+
+        block(database)
+
+      }
+    }
+  }
+}
 
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
  * For more information, consult the wiki.
  */
-class ApplicationSpec extends PlaySpec with OneAppPerTest {
+class ApplicationSpec extends PlaySpec with OneAppPerTest with BeforeAndAfter with WithTestDatabase {
+
+  // Override newAppForTest if you need a FakeApplication with other than non-default parameters
+  implicit override def newAppForTest(testData: TestData): Application = new
+      GuiceApplicationBuilder().loadConfig(new Configuration(ConfigFactory.load("application-testdb.conf"))).build()
+
+  before {
+
+  }
+
+  after {
+
+  }
 
   "Routes" should {
 
--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -1,4 +1,8 @@
+import com.typesafe.config.ConfigFactory
+import org.scalatest.{BeforeAndAfter, TestData}
 import org.scalatestplus.play._
+import play.api.{Application, Configuration}
+import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test._
 import play.api.test.Helpers._
 
@@ -6,7 +10,11 @@ import play.api.test.Helpers._
  * add your integration spec here.
  * An integration test will fire up a whole play application in a real (or headless) browser
  */
-class IntegrationSpec extends PlaySpec with OneServerPerTest with OneBrowserPerTest with HtmlUnitFactory {
+class IntegrationSpec extends PlaySpec with OneServerPerTest with BeforeAndAfter with WithTestDatabase with OneBrowserPerTest with HtmlUnitFactory {
+
+  // Override newAppForTest if you need a FakeApplication with other than non-default parameters
+  implicit override def newAppForTest(testData: TestData): Application = new
+      GuiceApplicationBuilder().loadConfig(new Configuration(ConfigFactory.load("application-testdb.conf"))).build()
 
   "Application" should {
 
